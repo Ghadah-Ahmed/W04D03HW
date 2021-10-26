fd88cec6f12e8b24c8036fb2b0b7173f761ae602
Ghadah-Ahmed <moslem.mah@gmail.com>
2021-10-26 13:35:53 +0300

    add styles + more functionalities

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,46 +1,93 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
+// class App extends React.Component {
 
+//   constructor(props) {
+//     super(props);
+//     this.state = { name: 'Ghadah' };
+//   }
+
+//   componentWillMount(){
+//    console.log('This is executed before rendering')
+//    console.log(ReactDOM.findDOMNode(this))
+//   }
+
+//   componentDidMount(){
+//     console.log('This is executed after rendering')
+//     console.log(ReactDOM.findDOMNode(this))
+//   }
+//   render() {
+//     return <h1>Hello, {this.state.name}</h1>;
+//   }
+// }
+
+
+function App() {
+  const [value, setValue] = React.useState("");
+  const [count, setCount] = React.useState(4);
   const [toDoList, setToDoList] = React.useState([
     {
-      task: '1',
+      id: 1,
+      task: 'My first task',
+      isDone: false
     },
     {
-      task: '2',
+      id: 2,
+      task: 'My second task',
+      isDone: false
     },
     {
-      task: '3',
+      id: 3,
+      task: 'My third task',
+      isDone: false
     }
   ]);
 
   function addTodo(newTask) {
-    var updatedList = [...toDoList, { task: newTask }];
-    setToDoList(updatedList);
+    setCount(count + 1)
+    setToDoList( [...toDoList, { task: newTask, id: count, isDone: false }] );
   }
-
-  const [value, setValue] = React.useState("");
 
   const handleSubmit = e => {
     e.preventDefault();
     if (!value) return;
     addTodo(value);
+    setValue(' ')
+  };
+
+  const deleteItem = id => {
+    setToDoList(toDoList.filter((task) => {
+      return task.id != id
+    }));
+  };
+
+  const completed = item => {
+    let index = toDoList.findIndex((task) => {
+      return task.id == item.id
+    }) 
+    const newTodos = [...toDoList];
+    newTodos[index].isDone ? newTodos[index].isDone = false : newTodos[index].isDone = true;
+    setToDoList(newTodos);
   };
 
   return (
     <div className="App">
       <h1>TO DO</h1>
         {toDoList.map((task) => (
-          <li>{task.task}</li>
+          <div className='app-div'>
+            <li style={{ textDecoration: task.isDone ? "line-through" : "" }} key={task.id}>{task.task}</li>
+            <button onClick={() => completed(task)}>COMPLETED</button> 
+            <span onClick={() => deleteItem(task.id)}>X</span>
+          </div>
         ))} 
 
     <form className='form' onSubmit={handleSubmit}>
         <label>What is need to be done?</label><br/>
-        <input
+        <input value={value} placeholder='write your task!'
         onChange={e => setValue(e.target.value)}
-        ></input> <br/>
+        /> <br/>
         <button type='submit'>ADD</button>
     </form>
     </div>
